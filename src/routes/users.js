c95d23ba92489e20d9404e28c7235aa617e103cf
DevCesarHdez <dev.cesar.hdez@gmail.com>
2020-08-23 21:14:27 -0500

    users placeholder

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
+const fetch = require('node-fetch')
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('users');
+router.get('/', async (req, res) => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = await response.json();
+  res.json({users});
 })
 
 module.exports = router;
